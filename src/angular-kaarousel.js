10de7d8b23b7c53d6534d44fca82f5fc6bb7921d
Corentin Gautier <co.gautier@gmail.com>
2014-08-28 02:26:12 +0200

    updated version

--- src/angular-kaarousel.js
+++ src/angular-kaarousel.js
@@ -18,21 +18,34 @@ angular.module('angular-kaarousel', [
         displayed: '=',
         perSlide: '=',
         autoplay: '=',
+        pauseOnHover: '=',
         centerActive: '=',
         timeInterval: '=',
         updateRate: '=',
         stopAfterAction: '=',
+        hideNav: '=',
+        hidePager: '=',
+        navOnHover: '=',
+        pagerOnHover: '=',
+        swipable: '=kaaSwipable',
+        sync: '=',
+        rtl: '=',
+        data: '='
       },
 
       controller: function ($scope) {
 
-        $scope.currentIndex = 0;
+        var self = this, conf;
 
         $scope.slides = [];
         $scope.elements = [];
 
+        $scope.currentIndex = 0;
+
         $scope.swipeThreshold = 50;
         $scope.swipeStageWidth = 200;
+
+        $scope.shouldAnim = false;
 
         $scope.userAction = false;
         $scope.margin = 0;
@@ -40,58 +53,88 @@ angular.module('angular-kaarousel', [
         $scope.kaarouselStyles = {
           'margin-left': '0px'
         };
-
-        var self = this, conf;
         
         self.getConf = function () {
-          conf = $scope.conf = {
-            displayed : Math.abs($scope.displayed),
-            perSlide : $scope.perSlide,
-            autoplay: $scope.autoplay || false,
+          conf = {
+            displayed : self.computeDisplayed(),
+            perSlide : self.computePerSlides(),
+            autoplay: $scope.autoplay,
+            pauseOnHover: $scope.pauseOnHover || false,
             centerActive: $scope.centerActive || false,
-            timeInterval: $scope.timeInterval || 5000,
-            stopAfterAction: $scope.stopAfterAction || false
+            timeInterval: $scope.timeInterval || 3000,
+            stopAfterAction: $scope.stopAfterAction || false,
+            hideNav: $scope.hideNav || false,
+            hidePager: $scope.hidePager || false,
+            navOnHover: $scope.navOnHover || false,
+            pagerOnHover: $scope.pagerOnHover || false,
+            swipable: $scope.swipable,
+            sync: $scope.sync,
+            rtl: $scope.rtl || false
           };
           return conf;
         };
 
-        self.getConf();
+        self.computeDisplayed = function () {
+          var displayed = $scope.displayed;
+          if ( displayed === undefined || isNaN(displayed) ) {
+            displayed = 3;
+          }
+          if ( displayed > $scope.elements.length ) {
+            displayed = $scope.elements.length;
+          }
+          return Math.abs(displayed);
+        };
+
+        self.computePerSlides = function () {
+          var perSlide = Math.abs($scope.perSlide);
+          if ( perSlide === undefined || isNaN(perSlide) ) {
+            perSlide = 1;
+          }
+          if ( $scope.rtl ) {
+            perSlide = - perSlide;
+          }
+          return perSlide;
+        };
+
+        conf = self.getConf();
 
         self.addSlide = function ( item, jElem ) {
           $scope.slides.push(item);
           $scope.elements.push(jElem);
-
           $scope.widths.push(angular.element(jElem).outerWidth());
+
+          if ( conf.rtl ) {
+            $scope.currentIndex = $scope.elements.length - 1;
+          }
         };
 
         self.computeIndex = function ( index ) {
           var nbItems = $scope.slides.length;
-
           if ( index >= nbItems ) { index = 0; }
-
           if ( index < 0 ) { index = nbItems - 1; }
-
           return index;
         };
 
         self.goPrev = function ( userAction, strength ) {
           var index = $scope.currentIndex - ( strength || parseInt(conf.perSlide, 10));
-          return self.navTo( index , userAction);
+          if ( userAction ) { 
+            $scope.userAction = true;
+          }
+          self.setInterval(self.shouldStop());
+          return self.goTo(self.computeIndex(index));          
         };
 
         self.goNext = function ( userAction, strength ) {
           var index = $scope.currentIndex + ( strength || parseInt(conf.perSlide, 10));
-          return self.navTo( index , userAction);
-        };
-
-        self.navTo = function ( index, ua ) {
-          if ( ua ) { $scope.userAction = true; }
+          if ( userAction ) { 
+            $scope.userAction = true;
+          }
           self.setInterval(self.shouldStop());
           return self.goTo(self.computeIndex(index));          
         };
 
         self.shouldStop = function () {
-          if ( $scope.autoplay ) {
+          if ( conf.autoplay ) {
             if ( ($scope.userAction && conf.stopAfterAction) || $scope.pausedByUser ) {
               return true;
             }
@@ -106,7 +149,7 @@ angular.module('angular-kaarousel', [
 
           $timeout( function () {
             $scope.currentIndex = index;
-            $scope.margin = index === 0 ? index : self.getMargin( index, max );
+            $scope.margin = self.getMargin( index, max );
             $scope.kaarouselStyles['margin-left'] = - $scope.margin + 'px';
           });
 
@@ -122,11 +165,25 @@ angular.module('angular-kaarousel', [
         };
 
         self.getMargin = function ( index, max ) {
-          var margin = 0, watchingUntil = self.getWatchUntil(index);
+          var nbItems = $scope.widths.length, 
+              watchingUntil = self.getWatchUntil(index),
+              margin = 0, j;
 
-          for ( var j = 0; j < $scope.widths.length; j++ ) {
-            if ( j < watchingUntil && j < max ) {
-              margin += $scope.widths[j];
+          if ( !conf.rtl ) {
+            for ( j = 0; j < $scope.widths.length; j++ ) {
+              if ( j < watchingUntil && j < max ) {
+                margin += $scope.widths[j];
+              }
+            }
+          } else {
+            // RTL handler
+            for ( var i = 0; i < nbItems; i++ ) {
+              margin += $scope.widths[i];
+            }
+            for ( j = nbItems; j > 0 ; j-- ) {
+              if ( ( nbItems - j - 2 < nbItems - watchingUntil ) ) {
+                margin -= $scope.widths[j - 1];
+              }
             }
           }
 
@@ -144,12 +201,13 @@ angular.module('angular-kaarousel', [
           $interval.cancel($scope.interval);
           $scope.playing = false;
 
-          if ( shouldStop ) { return; }
+          if ( shouldStop || conf.sync || conf.sync === 0 ) { return; }
 
           $scope.playing = true;
-          $scope.interval = $interval(function () {
+          $scope.interval = $interval( function () {
+            $scope.shouldAnim = true;
             self.goNext();
-          }, conf.timeInterval);          
+          }, conf.timeInterval);
         };
 
         self.updateWidths = function () {
@@ -167,21 +225,51 @@ angular.module('angular-kaarousel', [
         };
 
         self.lastOfItems = function () {
-          console.log('LAST ITEM IN NGREPEAT');
+          self.goTo(self.getCurrentIndex());
+          if ( conf.autoplay && !$scope.playing ) {
+            self.setInterval();
+          }
+        };
+
+        this.updateKaarousel = function () {
+          self.getConf();
+          self.setInterval( self.shouldStop() );
+          self.updateWidths();
+          self.goTo($scope.currentIndex);
         };
 
         $scope.pause = function () {
           $scope.pausedByUser = true;
           self.setInterval( true );
         };
+
         $scope.resume = function () {
           $scope.pausedByUser = false;
           self.setInterval( self.shouldStop() );
         };
-        this.updateKaarousel = function () {
-          self.setInterval( self.shouldStop() );
-          self.updateWidths();
-          self.goTo($scope.currentIndex);
+
+        $scope.mouseEnterCallback = function () {
+          if ( $scope.pauseOnHover ) {
+            $scope.pause();
+          }
+          if ( $scope.navOnHover ) {
+            $scope.hideNav = false;
+          }
+          if ( $scope.pagerOnHover ) {
+            $scope.hidePager = false;
+          }
+        };
+
+        $scope.mouseLeaveCallback = function () {
+          if ( !$scope.stopAfterHover && $scope.pauseOnHover ) { 
+            $scope.resume();
+          }
+          if ( $scope.navOnHover ) {
+            $scope.hideNav = true;
+          }
+          if ( $scope.pagerOnHover ) {
+            $scope.hidePager = true;
+          }
         };
 
       },
@@ -189,10 +277,6 @@ angular.module('angular-kaarousel', [
       link: function (scope, element, attrs, controller) {
 
         var windowTimeout, watchTimeout;
-
-        if ( scope.autoplay && !scope.playing ) {
-          controller.setInterval();
-        }
 
         // Update on window resize
         angular.element($window).resize(function () {
@@ -203,12 +287,18 @@ angular.module('angular-kaarousel', [
         });
 
         // Update when those guys change
-        scope.$watchCollection('[autoplay, displayed, centerActive, timeInterval, stopAfterAction]', function () {
+        scope.$watchCollection('[autoplay, displayed, centerActive, timeInterval, stopAfterAction, pauseOnHover, sync, rtl]', function () {
           $timeout.cancel(watchTimeout);
           watchTimeout = $timeout( function () {
-            controller.getConf();
             controller.updateKaarousel();
           }, scope.updateRate);
+        });
+
+        scope.$watch('sync', function () {
+          if ( scope.sync !== undefined && !isNaN(scope.sync) && scope.sync !== null ) {
+            scope.shouldAnim = true;
+            controller.goTo(scope.sync);
+          }
         });
 
       }
@@ -232,12 +322,8 @@ angular.module('angular-kaarousel', [
     return {
     
       require: '^kaarousel',
-      scope: true,
-    
       link: function (scope, element, attrs, controller) {
-        scope.wrapperElement = element;
-        scope.isMoving = false;
-
+        
         var startCoords, lastCoords;
 
         var shouldSwipe = function () {
@@ -247,6 +333,8 @@ angular.module('angular-kaarousel', [
         var getStrength = function () {
           return Math.floor( Math.abs( startCoords.x - lastCoords.x ) / scope.swipeStageWidth ) + 1;
         };
+
+        scope.wrapperElement = element;
 
         scope.swipeNext = function () {
           if ( shouldSwipe() ) {
@@ -268,20 +356,25 @@ angular.module('angular-kaarousel', [
 
         scope.swipeHandler = $swipe.bind(element, {
           start: function ( coords ) {
+            if ( !scope.swipable ) { return; }
             $timeout(function () {
               startCoords = coords;
               lastCoords = null;
-              scope.isMoving = true;              
+              scope.shouldAnim = false;
+              scope.dragging = true;
             });
           },
           move: function ( coords ) {
+            if ( !scope.swipable ) { return; }
             $timeout(function () {
               lastCoords = coords;
               scope.addSwipeOffset();
-              scope.isMoving = true;
+              scope.shouldAnim = false;
+              scope.dragging = true;
             });
           },
           end: function ( coords ) {
+            if ( !scope.swipable ) { return; }
             $timeout(function () {
               lastCoords = coords;
               var displacement = startCoords.x - lastCoords.x;
@@ -294,10 +387,12 @@ angular.module('angular-kaarousel', [
                   scope.swipePrev();
                 }
               }
-              scope.isMoving = false;
+              scope.shouldAnim = true;
+              scope.dragging = false;
             });
           },
           cancel: function () {
+            if ( !scope.swipable ) { return; }
             scope.resetSwipe();
           }
         });
@@ -311,8 +406,8 @@ angular.module('angular-kaarousel', [
   .directive('kaarouselSlider', function () {
     return {
       require: '^kaarousel',
-      scope: true,
       link: function (scope, element) {
+        // REGISTER ELEMENT
         scope.sliderElement = element;
       }
     };
@@ -323,12 +418,11 @@ angular.module('angular-kaarousel', [
     return {
       require: '^kaarousel',
       link: function (scope, element, attrs, controller) {
-        
+
+        // REGISTER ITEM
         controller.addSlide(scope.slide, element);
 
-        if ( scope.$last ) {
-          controller.lastOfItems();
-        }
+        if ( scope.$last ) { controller.lastOfItems(); }
 
         scope.itemWidth = function() {
           return 100 / controller.getConf().displayed;
@@ -344,8 +438,16 @@ angular.module('angular-kaarousel', [
   .directive('kaarouselNav', function () {
     return {
       require: '^kaarousel',
-      link: function (scope, element) {
+      link: function (scope, element, attrs, controller) {
+        // REGISTER ELEMENT
         scope.navElement = element;
+
+        scope.goPrev = function () {
+          controller.goPrev(true);
+        };
+        scope.goNext = function () {
+          controller.goNext(true);
+        };
       }
     };
   })
@@ -354,10 +456,8 @@ angular.module('angular-kaarousel', [
   .directive('kaarouselPrev', function () {
     return {
       require: '^kaarousel',
-      link: function (scope, element, attrs, controller) {
-        scope.goPrev = function () {
-          controller.goPrev(true);
-        };
+      link: function () {
+        // DO STUFF WHEN CLICK ON PREV 
       }
     };
   })
@@ -366,10 +466,8 @@ angular.module('angular-kaarousel', [
   .directive('kaarouselNext', function () {
     return {
       require: '^kaarousel',
-      link: function (scope, element, attrs, controller) {
-        scope.goNext = function () {
-          controller.goNext(true);
-        };
+      link: function () {
+        // DO STUFF WHEN CLICK ON NEXT 
       }
     };
   })
@@ -377,9 +475,10 @@ angular.module('angular-kaarousel', [
   // PAGER
   .directive('kaarouselPager', function () {
     return {
-      require: '^kaarousel',      
+      require: '^kaarousel',
       link: function (scope, element, attrs, controller) {
         scope.goTo = function ( index ) {
+          scope.userAction = true;
           controller.setInterval( controller.shouldStop() );
           controller.goTo( index );
         };
